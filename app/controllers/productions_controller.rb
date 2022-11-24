13d95a1e3fc924c3845cdefc74a9123c155fc8d6
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-24 15:26:33 +0300

    add Create action

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -12,4 +12,16 @@ class ProductionsController < ApplicationController
             render json: {error: "Production Not Found"}, status: :not_found
         end
     end
+
+
+    def create
+        production = Production.create(production_params)
+        render json: production, status: :created
+    end
+
+    private
+
+    def production_params
+        params.permit(:title, :genre, :budget, :image, :director, :ongoing)
+    end
 end
