594b672b67b5eef481114c27cc9fa84322f70994
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-24 13:53:56 +0300

    create seed and alter table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Production.destroy_all 
+CrewMember.destroy_all
+
+Production.create([
+    {title: "Carmen", genre: "Opera", budget: 80000, image: "https://www.mensjournal.com/wp-content/uploads/mf/_main2_conor_0.jpg?w=900&quality=86&strip=all", director: "Dana White", ongoing:true},
+    {title: "Cats", genre: "Musical", budget: 50000, image: "https://www.mensjournal.com/wp-content/uploads/mf/_main2_conor_0.jpg?w=900&quality=86&strip=all", director: "Dana White", ongoing: false},
+    {title: "Hamilton", genre: "Musical", budget: 100000, image: "https://www.mensjournal.com/wp-content/uploads/mf/_main2_conor_0.jpg?w=900&quality=86&strip=all", director: "Dana White", ongoing: true},
+    {title: "Hamlet", genre: "Shakespearean", budget: 70000, image: "https://www.mensjournal.com/wp-content/uploads/mf/_main2_conor_0.jpg?w=900&quality=86&strip=all", director: "Dana White", ongoing:false}
+])
+
+30.times{CrewMember.create(name:Faker::Name.name, job_title:'fighter', salary: 65000, production:Production.all.sample)}
