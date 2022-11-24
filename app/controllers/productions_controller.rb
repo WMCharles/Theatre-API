5b3105e1544b7858091ee87eb450669218f5ed5e
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-24 14:41:24 +0300

    add Production actions && routes

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,2 +1,15 @@
 class ProductionsController < ApplicationController
+
+    def index 
+        render json: Production.all, status: :ok
+    end
+
+    def show 
+        production = Production.find_by(id: params[:id])
+        if production
+            render json: production
+        else
+            render json: {error: "Production Not Found"}, status: :not_found
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :crew_members
-  resources :productions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :crew_members
+  resources :productions, only: [:index, :show]
 end
