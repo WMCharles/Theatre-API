d0e3853ab0681d0f8650ee47acf1286f84164542
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-25 09:44:37 +0300

    add custom validation

--- app/models/production.rb
+++ app/models/production.rb
@@ -1,4 +1,14 @@
 class Production < ApplicationRecord
+
     has_many :crew_members, :dependent => :delete_all
-    validates :title, :genre, :budget, :image, :director, :ongoing, presence: true
+
+    validates :title, presence: true
+    validate :no_more_musical
+
+    # Custom Validation
+    def no_more_musical 
+        if genre.downcase == "musical"
+            errors.add(:genre, "Sorry, no more musicals.")
+        end
+    end
 end
