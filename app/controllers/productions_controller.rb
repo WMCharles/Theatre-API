b5e5504a35774b671deacd15a0ce49a035b78532
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-25 09:31:15 +0300

    add validations

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -2,6 +2,7 @@ class ProductionsController < ApplicationController
 
     # Error messages
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     # GET /production
     def index 
@@ -16,12 +17,11 @@ class ProductionsController < ApplicationController
 
     # CREATE production
     def create
-        production = find_production
+        production = Production.create!(production_params)
         render json: production, status: :created
     end
 
     def update
-        # GET /production/:id
         production = find_production
         production.update(production_params)
         render json: production, status: :accepted
@@ -41,6 +41,10 @@ class ProductionsController < ApplicationController
 
     def render_not_found_response
         render json: {error: "Production Not Found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: {error: invalid.record.errors}, status: :unprocessable_entity
     end
 
     def find_production
--- app/models/production.rb
+++ app/models/production.rb
@@ -1,3 +1,4 @@
 class Production < ApplicationRecord
     has_many :crew_members, :dependent => :delete_all
+    validates :title, :genre, :budget, :image, :director, :ongoing, presence: true
 end
