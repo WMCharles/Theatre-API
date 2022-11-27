0eddabc317479920659cfb8444a340169c70dcf7
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-27 20:59:33 +0300

    add crew_member serializer

--- app/serializers/production_serializer.rb
+++ app/serializers/production_serializer.rb
@@ -1,5 +1,6 @@
 class ProductionSerializer < ActiveModel::Serializer
   attributes :id, :title, :genre, :budget, :image, :director, :summary
+  has_many :crew_members
 
   def summary
     "#{self.object.title}: #{self.object.director}"
